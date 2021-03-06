2f0ee0baabb94ba480419a455406d9639858ab43
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-03-06 12:42:55 -0500

    user api route

--- server/routes/userAPI.js
+++ server/routes/userAPI.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router()
 
+
 router.get('/', (req, res) => res.send("users routes"));
+
+router.post('/', (req, res) => {
+  res.send("users routes")
+});
 
 module.exports = router;
