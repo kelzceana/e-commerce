30d85717f85a5a73620448310e9a27b2262bf4a5
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-03-09 06:11:05 -0500

    setup signed json web tokens

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI:"mongodb+srv://Kelz123:@9JuHg2Z6jK-d-Q@e-commerce.ntcqn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  mongoURI:"mongodb+srv://Kelz123:@9JuHg2Z6jK-d-Q@e-commerce.ntcqn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  jwtSecret: "fullstackdeveloper"
 }
--- server/routes/userAPI.js
+++ server/routes/userAPI.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const config = require('../config/keys');
 
+
+const {check, validationResult} = require('express-validator');
+const User = require('../models/Users')
 
 router.get('/', (req, res) => res.send("users routes"));
 
@@ -9,14 +14,48 @@ router.post('/', [
   check("name", "name is required").not().isEmpty(),
   check("email", "Please enter your email").isEmail(),
   check("password", "Password is incorrect").isLength({min: 5})
-
-], (req, res) => {
+], 
+  async (req, res) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(400).json({error: errors.array()})
   }
-  console.log(req.body)
-  res.send("users routes")
+  try {
+    const { name, email, password} = req.body
+    let user = await User.findOne({ email:email });
+
+    if (user) {
+      return res.status(400).json({ errors: [{ msg: "User already exists" }]})
+    }
+    user = new User({
+      name,
+      email,
+      password
+    })
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt)
+    user.save();
+    // res.send("user created")
+
+    //signing and sending a web token
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+    jwt.sign(
+      payload, 
+      config.jwtSecret, 
+      {expiresIn: 3600 * 24},
+      (err, token) => {
+        if (err) throw err;
+        res.json({token})  
+      });
+
+  } catch (error) {
+    console.error(error);
+  }
+  
 });
 
 module.exports = router;
